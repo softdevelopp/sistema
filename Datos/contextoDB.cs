fd040b86c2b3f0468823e51a4b279a4af9fda200
softdevelopp <softdevelopp@gmail.com>
2022-09-20 21:41:20 -0500

    Add project files.

---
+++ Datos/contextoDB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using sistema.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace sistema.Datos
+{
+    public class contextoDB:DbContext
+    {
+        public contextoDB(DbContextOptions<contextoDB>options):base(options)
+        {
+
+        }
+        public DbSet<cita>citas { get; set; }
+
+    }
+}
